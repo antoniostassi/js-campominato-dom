1f4a5ca77e81441e621ab54e4ff5952451e9604b
antoniostassi <stassiant@outlook.it>
2024-07-23 12:34:41 +0200

    JS | Win and Lose Condition

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,20 @@
 
 const start = document.getElementById("start");
 const container = document.querySelector(".container");
+let clickCount = 1;
+const bombsContainer = [];
+let gameStatus = true;
 
 start.addEventListener("click", function() {
 
     const difficulty = document.getElementById("difficulty").value;
     container.innerHTML = "";
+    generateBombs(difficulty);
     
     for (let i=0; i < difficulty; i++) {
-        // console.log("Create "+i);
         createNewElement("div", "square", container, "my-col-"+difficulty, i+1, "new-bg-square");
     }
 
-    generateBombs(difficulty);
 
 });
 
@@ -23,22 +25,31 @@ function createNewElement(tagElement, classToAdd, appendTag, gameDifficulty, num
     newElement.classList.add(classToAdd);
     newElement.classList.add(gameDifficulty);
     appendTag.append(newElement);
+    newElement.innerHTML = numberToDisplay;
 
     newElement.addEventListener("click", function() {
+    if (gameStatus) {
+        
         newElement.classList.add(toggleClass);
+        if (bombsContainer.includes(numberToDisplay)) {
+            alert("HAI PERSO! PUNTEGGIO: " +clickCount);
+            gameStatus = !gameStatus;
+        } else if(clickCount == (gameDifficulty-90)) { 
+            alert("HAI VINTO! PUNTEGGIO: " + clickCount);
+            gameStatus = !gameStatus;
+        } else {
+            clickCount++;
+        }
+    }
+        
     });
-
-    newElement.innerHTML = numberToDisplay;
 
 }
 
-const bombsContainer = [];
 
 function generateBombs(difficulty) {
 
-    let i=0;
-
-    while(i < 16) {
+    for(let i=0; i<16; i) {
 
         let num = Math.floor(Math.random() * difficulty) + 1;
 
